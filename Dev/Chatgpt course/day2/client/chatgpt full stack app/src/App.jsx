1387cb9f9b8f8efa349dcc4f467383efc8947846
Sai Yashwanth <saiyashwanth@Sais-MacBook-Air.local>
2023-07-18 18:08:30 +0530

    init setup

---
+++ Dev/Chatgpt course/day2/client/chatgpt full stack app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Header /> */}
+      <Home />
+    </>
+  )
+}
+
+export default App;
---
+++ Dev/Chatgpt course/day2/client/chatgpt full stack app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="">
+            <img src="/src/assets/react.svg" alt="logo" />
+            <h3>GPTbot</h3>
+        </div>
+    )
+}
+
+export default Header;
